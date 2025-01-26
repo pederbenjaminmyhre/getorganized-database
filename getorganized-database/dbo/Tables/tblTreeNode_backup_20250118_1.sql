16fa7ed73e6868cfbc4dc59fe7a42318fda09777
jonmayor <jon_benton_mayor@outlook.com>
2025-01-25 18:07:40 -0600

    re-added files

--- getorganized-database/dbo/Tables/tblTreeNode_backup_20250118_1.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[tblTreeNode_backup_20250118] (
-    [id]           BIGINT         IDENTITY (1, 1) NOT NULL,
-    [name]         NVARCHAR (100) NOT NULL,
-    [parentId]     BIGINT         NOT NULL,
-    [hasChildren]  BIT            NOT NULL,
-    [detailedText] NVARCHAR (MAX) NULL,
-    [customSort]   BIGINT         NULL
-);
-
