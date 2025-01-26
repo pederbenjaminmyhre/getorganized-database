0d5b4c1aa6fb6b906b49f7e599c53fc988aed1ec
jonmayor <115034432+jonmayor@users.noreply.github.com>
2025-01-25 18:06:24 -0600

    Delete getorganized-database/dbo/Tables directory

--- getorganized-database/dbo/Tables/tblTreeNode_DeleteLog.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[tblTreeNode_DeleteLog] (
-    [id]           BIGINT         NOT NULL,
-    [name]         NVARCHAR (100) NOT NULL,
-    [parentId]     BIGINT         NOT NULL,
-    [hasChildren]  BIT            NOT NULL,
-    [detailedText] NVARCHAR (MAX) NULL,
-    [customSort]   BIGINT         NULL
-);
-
