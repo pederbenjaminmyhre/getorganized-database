f6a8a6be124edae536bfae2b2db64a48b53f0435
jonmayor <jon_benton_mayor@outlook.com>
2025-01-25 19:42:30 -0600

    re-add objects

---
+++ getorganized-database/dbo/Stored Procedures/tblTreeNode_Insert.sql
@@ -0,0 +1,35 @@
+﻿CREATE PROCEDURE [dbo].[tblTreeNode_Insert]
+    @ParentId BIGINT = 0 -- Optional parameter, defaults to 0
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Insert the new record
+    INSERT INTO dbo.tblTreeNode (name, parentId, hasChildren, detailedText, customSort)
+    VALUES ('New Node', @ParentId, 0, NULL, NULL);
+
+	UPDATE dbo.tblTreeNode SET name = 'New Node ' + CAST(SCOPE_IDENTITY() AS NVARCHAR(100))
+	where id = SCOPE_IDENTITY();
+
+	IF @ParentId <> 0
+	BEGIN
+		UPDATE dbo.tblTreeNode
+		SET hasChildren = 1
+		WHERE id = @ParentId
+	END
+
+    -- Return the newly inserted record
+    SELECT 
+        id,
+        name,
+        parentId,
+        hasChildren,
+        detailedText,
+        customSort
+    FROM 
+        dbo.tblTreeNode
+    WHERE 
+        id = SCOPE_IDENTITY();
+END;
+
+
---
+++ getorganized-database/dbo/Tables/tblTreeNode_DeleteLog.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblTreeNode_DeleteLog] (
+    [id]           BIGINT         NOT NULL,
+    [name]         NVARCHAR (100) NOT NULL,
+    [parentId]     BIGINT         NOT NULL,
+    [hasChildren]  BIT            NOT NULL,
+    [detailedText] NVARCHAR (MAX) NULL,
+    [customSort]   BIGINT         NULL
+);
+
